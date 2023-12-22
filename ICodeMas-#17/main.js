a0d8b47917ba2bcba795956baad23d3ea440274d
= <abhisingh30800@gmail.com>
2023-12-22 12:59:53 +0530

    added ICodeMas #22

--- ICodeMas-#17/main.js
+++ ICodeMas-#17/main.js
@@ -1,35 +1,33 @@
-
 let itemData = [
-    {
-        name: "Gingerbread Man Cookie",
-        price: 12.99
-    },
-    {
-        name: "Star-shaped Cookie",
-        price: 12.99
-    },
-    {
-        name: "Christmas Tree Cookie",
-        price: 13.99
-    },
-    {
-        name: "Chocolate Chip Cookie",
-        price: 11.99
-    },
-    {
-        name: "Oatmeal Raisin Cookie",
-        price: 14.99
-    }
-]
-const itemsWrapper = document.querySelector('.items--wrapper');
-const searchString = document.getElementById('search--input');
-const quantity = document.querySelector('.quantity');
-
+  {
+    name: "Gingerbread Man Cookie",
+    price: 12.99,
+  },
+  {
+    name: "Star-shaped Cookie",
+    price: 12.99,
+  },
+  {
+    name: "Christmas Tree Cookie",
+    price: 13.99,
+  },
+  {
+    name: "Chocolate Chip Cookie",
+    price: 11.99,
+  },
+  {
+    name: "Oatmeal Raisin Cookie",
+    price: 14.99,
+  },
+];
+const itemsWrapper = document.querySelector(".items--wrapper");
+const searchString = document.getElementById("search--input");
+const quantity = document.querySelector(".quantity");
 
 const populateItems = (data) => {
-    itemsWrapper.innerHTML = data.map((item) => {
-        return (
-            `
+  itemsWrapper.innerHTML = data
+    .map((item) => {
+      return `
         <div class="item">
             <div class="item--info">
             <span class="block"></span>
@@ -37,21 +35,21 @@ const populateItems = (data) => {
             </div>
             <span class="price">$${item.price}</span>
           </div>
-            `
-        )
-    }).join("");
-    quantity.textContent = data.length;
-}
+            `;
+    })
+    .join("");
+  quantity.textContent = data.length;
+};
 populateItems(itemData);
 
-
 searchString.oninput = (e) => {
-    const { value } = e.target;
-    const filteredData = itemData.filter(item => item.name.toLowerCase().includes(value.toLowerCase()));
-    populateItems(filteredData);
-}
+  const { value } = e.target;
+  const filteredData = itemData.filter((item) =>
+    item.name.toLowerCase().includes(value.toLowerCase()),
+  );
+  populateItems(filteredData);
+};
 
-
-document.querySelector('form').onsubmit = (e) => {
-    e.preventDefault();
-}
+document.querySelector("form").onsubmit = (e) => {
+  e.preventDefault();
+};
