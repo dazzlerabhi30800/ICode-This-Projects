bcc061e3f35948d939a2c5102d7f8d55cb93c9d3
= <abhisingh30800@gmail.com>
2024-03-29 11:43:02 +0530

    added Movie Card

---
+++ Movie Card/index.js
@@ -0,0 +1,44 @@
+const movieInfoWrapper = document.querySelector(".movie--info");
+
+const movieData = [
+  {
+    title: "Actors",
+    target: "actor",
+    names: [
+      "Oscar Isaac",
+      "Mark Hamil",
+      "Daisy Ridley",
+      "John Boyega",
+      "Kelly Marie Tran",
+    ],
+  },
+  {
+    title: "Director",
+    target: "director",
+    names: ["Rion Johnson"],
+  },
+  {
+    title: "ScreenWriter",
+    target: "screenwriter",
+    names: ["Rion Johnson"],
+  },
+  {
+    title: "Producers",
+    target: "producer",
+    names: ["Kathleen Kennedy Johnson", "Ram Bergman"],
+  },
+];
+
+movieInfoWrapper.innerHTML = movieData
+  .map(
+    (info) =>
+      `
+    <div class="movie--container ${info.target}">
+    <h3>${info.title}</h3>
+    <ul>
+    ${info.names.map((name) => `<li>${name}</li>`).join("")}
+    </ul>
+   </div> 
+    `
+  )
+  .join("");
